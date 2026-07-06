76dfbd92bb7c1a7bc8eefb0424ab00cfa38ed3a7
cmyk-p4nd4 <70799443+cmyk-p4nd4@users.noreply.github.com>
2026-07-06 15:35:50 +1000

    Modified Parser example and added pip requirements

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import socket
 import struct
 import numpy as np
+from typing import Union, Optional
+from pathlib import Path
 import zstandard as zstd
+from zstandard import ZstdDecompressor
 
 DEFAULT_ADDR = ""
 DEFAULT_PORT = 5005
@@ -13,6 +16,7 @@ NEW_EXAMPLE_FILE = "cam_radar_1783260788477740516.dat"
 
 
 reciever = (DEFAULT_ADDR, DEFAULT_PORT)
+
 
 # IPv6 UDP server setup (for recieving radar packets)
 def udp_radar_reciever(port: int = DEFAULT_PORT):
@@ -36,18 +40,62 @@ def udp_radar_reciever(port: int = DEFAULT_PORT):
 
 
 # For manually reading a data file into the server
-def import_data_from_desktop(file_name: str):
-    time = int(file_name.strip('abcdefghijklmnopqrstuvwxyz. _'))
-    with zstd.open(file_name) as f:
-        data = f.read(7)
-        duration = struct.unpack('<I',data[2:6])*1000
-        message_type = data[6]
+def import_data_from_desktop(file_name: Union[Path, str]):
+    if isinstance(file_name, str):
+        file = Path(file_name).resolve()
+    elif isinstance(file_name, Path):
+        file = file_name.resolve()
+    else:
+        raise RuntimeError("Invalid Argument")
 
-        data = f.read()
-    length = len(data)
-    print(length)
-    points = np.frombuffer(data[:length - (length % 6)], dtype=np.uint16).reshape(-1, 3) 
-    return time, duration[0], message_type, points
+    if not file.exists() or file.suffix != ".dat":
+        raise FileNotFoundError("The provided file does not match it's intended usage")
+
+    HEADER = b"::"
+    FOOTER = b";;"
+    METADATA_SIZE = 7
+
+    time = int(file.stem.strip("abcdefghijklmnopqrstuvwxyz. _"))
+    with file.open("rb") as f:
+        decoder = ZstdDecompressor()
+        decompressed_data = b"".join(decoder.read_to_iter(f.read()))
+
+    cursor = 0
+    data_len = len(decompressed_data)
+
+    while cursor < data_len:
+        # Find the next header start
+        header_idx = decompressed_data.find(HEADER, cursor)
+        if header_idx == -1:
+            break  # EOF
+
+        # Find the corresponding footer
+        footer_idx = decompressed_data.find(FOOTER, header_idx)
+        if footer_idx == -1:
+            print(f"Warning: Found header at {header_idx} but no matching footer.")
+            break
+
+        # Extract Metadata (2 + 4 + 1)
+        try:
+            # throw away the header
+            _, timestamp_us, message_type = struct.unpack(
+                "<2sIB", decompressed_data[header_idx : header_idx + METADATA_SIZE]
+            )
+        except struct.error as e:
+            print(f"Error parsing metadata block at index {header_idx}: {e}")
+            break
+
+        payload_start = header_idx + METADATA_SIZE
+        payload_end = footer_idx
+        pc_payload = decompressed_data[payload_start:payload_end]
+
+        point_cloud_np = np.frombuffer(pc_payload, dtype=np.int16) / 1000
+        point_cloud_np = point_cloud_np.reshape((-1, 3))
+
+        # skip footer
+        cursor = footer_idx + 2
+        yield time, timestamp_us, message_type, points
+
 
 if __name__ == "__main__":
     time, duration, mes_type, points = import_data_from_desktop(NEW_EXAMPLE_FILE)
